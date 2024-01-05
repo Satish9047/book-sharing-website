4b4a00ab04d44d84cd3fb07e5dcc031b11dc7f3e
Satish9047 <satishprajapati930@gmail.com>
2024-01-05 09:17:22 +0545

    jsonwebtoken and bcrypt is added

--- server/src/repositories/auth.ts
+++ server/src/repositories/auth.ts
@@ -1,7 +1,11 @@
-export const loginHandler = async()=>{
-    return "hello auth login from the repositories";
-};
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const registerHandler = async()=>{
     return "hello auth register from the repositories";
 };
+
+export const loginHandler = async()=>{
+    return "hello auth login from the repositories";
+};
+
