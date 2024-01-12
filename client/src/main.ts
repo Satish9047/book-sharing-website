5d9271ac865ec55a9715cc34b85f78b9191480dd
Satish9047 <satishprajapati930@gmail.com>
2024-01-12 21:13:22 +0545

    add comments

--- client/src/main.ts
+++ client/src/main.ts
@@ -19,92 +19,6 @@ const state = {
 //search value
 let value = "";
 
-
-//rendering the search by div
-for (let i = 0; i < searchLabel.length; i++) {
-    const div = document.createElement("div") as HTMLDivElement;
-    div.classList.add("p-2", "flex", "gap-2", "bg-[#F3F3F3]", "shadow-lg", "rounded-md");
-
-    const input = document.createElement("input") as HTMLInputElement;
-    input.type = "checkbox";
-
-    const label = document.createElement("label") as HTMLLabelElement;
-    label.innerText = searchLabel[i];
-    div.appendChild(input);
-    div.appendChild(label);
-    searchByList.appendChild(div);
-
-    input.addEventListener("change", async () => {
-        // console.log(searchLabel[i], input.checked);
-        state[searchLabel[i]] = input.checked;
-        console.log(state);
-        // await getBook();
-    });
-}
-
-
-
-//adding event listeners to the input field
-searchInputElement.addEventListener("keydown", async(ev)=>{
-    if(ev.key === "Enter"){
-        value = searchInputElement.value;
-        console.log("enter is clicked", value);
-        const res = await getBook();
-        console.log(res);
-
-        const bookItems = res.data;
-        bookItemsElement.innerHTML = "";
-
-        //rendering the list of books in the dom
-        bookItems.forEach((bookItem:IBook)=>{
-            const div = document.createElement("div") as HTMLDivElement;
-            div.classList.add("p-2", "flex", "gap-2", "justify-between", "bg-[#F3F3F3]", "shadow-lg", "rounded-md");
-
-            const heading = document.createElement("h1") as HTMLHeadingElement;
-            heading.innerText = bookItem.book_name;
-            div.appendChild(heading);
-
-            const paragraph= document.createElement("p") as HTMLParagraphElement;
-            paragraph.innerText = bookItem.author_name;
-            div.appendChild(paragraph);
-
-            const figure = document.createElement("figure");
-            figure.classList.add("w-[30px]");
-
-            const downloadImage = document.createElement("img");
-            downloadImage.src = "../../public/icon/download-icon.png";
-            downloadImage.classList.add("w-[30px]");
-            
-            figure.appendChild(downloadImage);
-            div.appendChild(heading);
-            div.appendChild(paragraph);
-            div.appendChild(figure);
-
-            //for download event listeners
-            figure.addEventListener("click", async () => {
-                
-                //console.log(book);
-                try {
-                    //sending the request to download book pdf
-                    const res = await HTTP.get(`/books/download/${bookItem.book_id}`, { responseType: "arraybuffer" });
-                    console.log(res);
-                    if (res.status === 200) {
-                        const blob = new Blob([res.data], { type: "application/pdf" });
-                        const link = document.createElement("a");
-                        link.href = window.URL.createObjectURL(blob);
-                        link.download = "bookhome.pdf";
-                        link.click();
-                    }
-                } catch (error) {
-                    console.log(error);
-                }
-            });
-            bookItemsElement.appendChild(div);
-        });
-    }
-});
-
-
 //onload event listeners
 window.addEventListener("load", async () => {
 
@@ -170,6 +84,101 @@ window.addEventListener("load", async () => {
     }
 });
 
+
+//rendering the searchBy div
+for (let i = 0; i < searchLabel.length; i++) {
+    const div = document.createElement("div") as HTMLDivElement;
+    div.classList.add("p-2", "flex", "gap-2", "bg-[#F3F3F3]", "shadow-lg", "rounded-md");
+
+    const input = document.createElement("input") as HTMLInputElement;
+    input.type = "checkbox";
+
+    const label = document.createElement("label") as HTMLLabelElement;
+    label.innerText = searchLabel[i];
+    div.appendChild(input);
+    div.appendChild(label);
+    searchByList.appendChild(div);
+
+    input.addEventListener("change", async () => {
+        // console.log(searchLabel[i], input.checked);
+        state[searchLabel[i]] = input.checked;
+        console.log(state);
+        // await getBook();
+    });
+}
+
+
+
+//adding event listeners to the input field
+searchInputElement.addEventListener("keydown", async(ev)=>{
+    if(ev.key === "Enter"){
+        value = searchInputElement.value;
+        console.log("enter is clicked", value);
+        const res = await getBook();
+        console.log(res);
+
+        const bookItems = res.data;
+        bookItemsElement.innerHTML = "";
+
+        //rendering the list of books in the dom
+        bookItems.forEach((bookItem:IBook)=>{
+
+            //create the html element
+            const div = document.createElement("div") as HTMLDivElement;
+            div.classList.add("p-2", "flex", "gap-2", "justify-between", "bg-[#F3F3F3]", "shadow-lg", "rounded-md");
+
+            const heading = document.createElement("h1") as HTMLHeadingElement;
+            heading.innerText = bookItem.book_name;
+            div.appendChild(heading);
+
+            const paragraph= document.createElement("p") as HTMLParagraphElement;
+            paragraph.innerText = bookItem.author_name;
+            div.appendChild(paragraph);
+
+            const figure = document.createElement("figure");
+            figure.classList.add("w-[30px]");
+
+            const downloadImage = document.createElement("img");
+            downloadImage.src = "../../public/icon/download-icon.png";
+            downloadImage.classList.add("w-[30px]");
+            
+
+            //appending the html element into the div
+            figure.appendChild(downloadImage);
+            div.appendChild(heading);
+            div.appendChild(paragraph);
+            div.appendChild(figure);
+
+            bookItemsElement.appendChild(div);
+
+            //for download event listeners
+            figure.addEventListener("click", async () => {
+                
+                //console.log(book);
+                try {
+                    //sending the request to download book pdf
+                    const res = await HTTP.get(`/books/download/${bookItem.book_id}`, { responseType: "arraybuffer" });
+                    console.log(res);
+                    if (res.status === 200) {
+                        const blob = new Blob([res.data], { type: "application/pdf" });
+                        const link = document.createElement("a");
+                        link.href = window.URL.createObjectURL(blob);
+                        link.download = "bookhome.pdf";
+                        link.click();
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+
+
+            //redirecting to the book detail page
+            heading.addEventListener("click", () => {
+                window.location.href = "../view/book/book.html?" + "bookId=" + bookItem.book_id;
+            });
+        });
+    }
+});
 
 
 /**
